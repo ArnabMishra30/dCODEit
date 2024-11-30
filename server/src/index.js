fdb1b44efaef2ac7d8077e35b85da7ab649187f2
Arnab Mishra <mishraarnab07@gmail.com>
2024-11-30 11:57:20 +0530

    changed port to 4000

--- server/src/index.js
+++ server/src/index.js
@@ -18,7 +18,7 @@ connectDB()
         // app.listen(process.env.PORT || 8000), () => {
         //     console.log(`Server is running on port : ${process.env.PORT}`)
         // }
-        const PORT = process.env.PORT || 8000;
+        const PORT = process.env.PORT || 4000;
         app.listen(PORT, () => {
             console.log(`Server is running on port: ${PORT}`);
         });
